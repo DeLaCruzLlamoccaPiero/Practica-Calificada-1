4f48f4a75ad426a6005877d3cad1c357ca9d2026
Piero De la Cruz <>
2026-04-24 18:06:50 -0500

    Nuevo Repositorio

---
+++ PracticaCalificada01/src/practicacalificada01/PracticaCalificada01.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicacalificada01;
+
+/**
+ *
+ * @author HP
+ */
+public class PracticaCalificada01 {
+    public static void main(String[] args) {
+       
+        System.out.println("holaaaa mundodd");    
+        
+        
+        
+        
+        
+       
+        
+        
+        
+    }
+}
