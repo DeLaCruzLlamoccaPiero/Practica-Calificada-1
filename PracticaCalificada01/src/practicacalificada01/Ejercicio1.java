d25a4a015a5edbeb6450f2bd4be63afc8955ebf9
Piero De La Cruz <>
2026-04-25 19:26:10 -0500

    Resolucion 1

--- PracticaCalificada01/src/practicacalificada01/Ejercicio1.java
+++ PracticaCalificada01/src/practicacalificada01/Ejercicio1.java
@@ -3,7 +3,65 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package practicacalificada01;
+import java.util.Scanner;
 
-public class Ejercicio1 {}
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ingrese las horas (0-23)");
+        int horas = scanner.nextInt();
+                
+        System.out.println("Ingrese los minutos (0-59)");
+        int minutos = scanner.nextInt();
+                
+        System.out.println("Ingrese los segundos (0-59)");
+        int segundos = scanner.nextInt();
+        
+        if (horas <0 || horas >23){
+            System.out.println("Error: Las horas deben estar entre 0 y 23");
+            scanner.close();
+            return;
+        }
+        if (minutos <0 || minutos > 59) {
+            System.out.println("Error");
+            scanner.close();
+            return;
+        }    
+        if (segundos <0 || segundos >59) {
+            System.out.println("Error");
+            scanner.close();
+            return;
+        }
+        
+        segundos++;
+        
+        if (segundos >59){
+            segundos=0;
+            minutos++;
+        }
+        
+        if (minutos >59 ){
+            minutos=0;
+            horas++;
+        }         
+        
+        if (horas >23 ){
+            horas=0;
+        }         
+        
+        System.out.printf("Salida: %02d*%02d:%02dn", horas, minutos, segundos);
+        
+        scanner.close();
+        
+ 
+    }
+}
+
+  
+        
+ 
+
     
      
